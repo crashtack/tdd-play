e718d64167eb372cf1292281b087e8c664131655
crashtack <crashtack@gmail.com>
2016-08-19 13:00:40 -0700

    test_ack.py

--- src/test_ack.py
+++ src/test_ack.py
@@ -23,22 +23,11 @@ ACK_TABLE = [
     (3, 3, 61),
     (3, 4, 125)
 ]
-
 BAD_TABLE = [
     ('a', 'b'),
     (-1, 'c'),
     (-1, 1),
     ('c', 1)
-]
-
-VAL_TESTS = [
-    (1, True),
-    (10, True),
-    (1.75, False),
-    (-1, False),
-    (-1.75, False),
-    ('a', False),
-    ([], False),
 ]
 
 
@@ -47,15 +36,9 @@ def test_akermann(m, n, result):
     from ackermann import ackermann
     assert ackermann(m, n) == result
 
-#
-# @pytest.mark.parametrize('m, n', BAD_TABLE)
-# def test_bad_inputs(m, n):
-#     from ackermann import ackermann
-#     with pytest.raises(ValueError):
-#         ackermann(m, n)
-#
-#
-# @pytest.mark.parametrize('val, result', VAL_TESTS)
-# def test_valid_value(val, result):
-#     from ackermann import is_positive_int
-#     assert is_positive_int(val) == result
+
+@pytest.mark.parametrize('m, n', BAD_TABLE)
+def test_bad_inputs(m, n):
+    from ackermann import ackermann
+    with pytest.raises(ValueError):
+        ackermann(m, n)
